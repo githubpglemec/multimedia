a2b713c9615732066446f30655b4d344c1ead544
githubpglemec <108912874+githubpglemec@users.noreply.github.com>
2024-01-07 01:40:24 -0800

    Add files via upload

---
+++ image.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np 
+img=cv2.imread('cat.jpg')
+cv2.imshow('before',img)
+#img=cv2.resize(img,(400,500))p
+
+image=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+(thresh,bimage1)=cv2.threshold(image,127,255,cv2.THRESH_BINARY)
+
+(thresh,bimage2)=cv2.threshold(image,150,200,cv2.THRESH_BINARY)
+
+#both_image= np.hstack([image,bimage2,bimage2])
+
+cv2.imshow('Black&White_image_width_1st_threshold_value',bimage1)
+cv2.waitKey(0)
+cv2.imshow('Black&White_image_width_2nd_value',bimage2)
+cv2.waitKey(0)
+cv2.destroyallWindows()
+
